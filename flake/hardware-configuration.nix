2e5745af6e929669037d573ce2f6016f628af65a
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-12-12 18:44:14 +0000

    redeploy new mantis

--- flake/hardware-configuration.nix
+++ flake/hardware-configuration.nix
@@ -1,5 +1,5 @@
-{ modulesPath, ... }: {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+{modulesPath, ...}: {
+  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
@@ -9,9 +9,8 @@
     device = "/dev/disk/by-uuid/DF18-CEB6";
     fsType = "vfat";
   };
-  boot.initrd.availableKernelModules =
-    [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
-  boot.initrd.kernelModules = [ "nvme" ];
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
+  boot.initrd.kernelModules = ["nvme"];
   fileSystems."/" = {
     device = "/dev/sda1";
     fsType = "ext4";
--- flake/host.nix
+++ flake/host.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
