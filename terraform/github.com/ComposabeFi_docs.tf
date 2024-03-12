12d033fcae9514429f06f58e9afcd459d24b99cb
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-12 10:53:39 +0000

    feat: added docs repo

---
+++ terraform/github.com/ComposabeFi_docs.tf
@@ -0,0 +1,22 @@
+
+resource "github_repository" "docs" {
+  name        = "docs"
+  description = "Docs which are not specific to any underlying coding domain (biz side)"
+
+  has_issues = false
+  has_discussions = false
+  has_projects = false
+  has_wiki = false
+  is_template = false
+  delete_branch_on_merge = true
+  visibility = "public"
+}
+
+resource "github_repository_collaborators" "docs" {
+  repository = github_repository.docs.name
+  user {
+    permission = "admin"
+    username   = data.github_user.docs.username
+  }
+}
+
